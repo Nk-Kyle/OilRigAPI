a52af25d1bde46f7d7adf7a9e73a59327dfa0774
Kyle <kyle.treoxer@gmail.com>
2023-11-21 14:47:20 +0700

    feat: assignment provided id

--- blueprints/assignment/views.py
+++ blueprints/assignment/views.py
@@ -7,16 +7,10 @@ import uuid
 assignment = Blueprint("assignment", __name__)
 
 
-def get_seq(prefix: str):
-    res = db.counters.find_one_and_update(
-        {"_id": prefix}, {"$inc": {"seq": 1}}, return_document=True
-    )
-    return res["seq"]
-
-
 @assignment.route("/", methods=["POST", "GET", "PUT", "DELETE"])
 def assignment_view():
     if request.method == "POST":
+        assignment_id = request.json.get("assignment_id", "")
         level_id = request.json.get("level_id", "")
         level_name = request.json.get("level_name", "")
         location_id = request.json.get("location_id", "")
@@ -25,13 +19,13 @@ def assignment_view():
         creator = request.json.get("creator", "")
         division = request.json.get("division", "")
         work_type = request.json.get("work_type", "")
+        priority = request.json.get("priority", 0)
         description = request.json.get("description", "")
-        prefix = request.json.get("prefix", "")
 
         try:
             res = db.assignments.insert_one(
                 {
-                    "_id": prefix + str(get_seq(prefix)),
+                    "_id": assignment_id,
                     "division": division,
                     "work_type": work_type,
                     "level_id": level_id,
@@ -43,6 +37,7 @@ def assignment_view():
                     "description": description,
                     "status": "TO DO",
                     "progress": 0,
+                    "priority": priority,
                     "logs": [],
                 }
             )
@@ -67,24 +62,26 @@ def assignment_view():
             "data": data,
         }, 200
 
-    # elif request.method == "DELETE":
-    #     id = request.json.get("id", "")
-    #     # Delete level if it exists else return 404
-    #     res = db.levels.delete_one({"_id": ObjectId(id)})
-    #     if res.deleted_count == 1:
-    #         return {"status": 200}, 200
-    #     else:
-    #         return {"status": 404}, 404
-    # elif request.method == "PUT":
-    #     id = request.json.get("id", "")
-    #     name = request.json.get("name", "")
-    #     img_url = request.json.get("img_url", "")
-    #     # Update level if it exists else return 404
-    #     res = db.levels.update_one(
-    #         {"_id": ObjectId(id)},
-    #         {"$set": {"name": name, "img_url": img_url}},
-    #     )
-    #     if res.modified_count == 1:
-    #         return {"status": 200}, 200
-    #     else:
-    #         return {"status": 404}, 404
+
+@assignment.route("/<assignment_id>", methods=["GET", "PUT", "DELETE"])
+def single_assignment(assignment_id):
+    if request.method == "GET":
+        res = db.assignments.find_one({"_id": assignment_id})
+        if res is None:
+            return {"status": 404}, 404
+
+        res["id"] = str(res["_id"])
+        del res["_id"]
+
+        return {
+            "status": 200,
+            "data": res,
+        }, 200
+
+    elif request.method == "DELETE":
+        res = db.assignments.delete_one({"_id": assignment_id})
+
+        if res.deleted_count == 0:
+            return {"status": 404}, 404
+
+        return {"status": 200}, 200
